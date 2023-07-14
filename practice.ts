15ac248ed4c32de42e7d8b83c91794e94d956cbd
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-07-14 17:33:47 -0400

    type shapes and any

--- practice.ts
+++ practice.ts
@@ -1,4 +1,4 @@
-
+// Type Interfaces
 let aged = true;
 let realAge = 0;
 
@@ -9,3 +9,19 @@ if (aged) {
 let dogAge = realAge * 7;
 
 console.log(`${dogAge} years`);
+
+// Type Shapes
+let firstName = 'muriel!';
+
+console.log(firstName.toUpperCase());
+
+console.log(firstName.length);
+
+// Any
+
+let guess;
+
+guess = "green";
+console.log(guess);
+guess = 1;
+console.log(guess);
