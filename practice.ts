80a3d0d814d266dbeb9ea445769d8e615e8b3ff1
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-07-20 19:39:04 -0400

    arrays

--- practice.ts
+++ practice.ts
@@ -92,4 +92,10 @@ function printRandomNumber() : void{
 
 printRandomNumber();
 
+// Arrays:
+let bestNumbers: number[] = [7,77,4];
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[] = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];
+
 export {}
