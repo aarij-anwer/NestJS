4f563950322c3167c53855658e08af1430a173b1
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-07-20 20:40:36 -0400

    Multidimensional arrays

--- practice.ts
+++ practice.ts
@@ -98,4 +98,10 @@ let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
 let bestBreakfasts: string[] = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
 let bestBooleans: boolean[] = [true, false];
 
+
+// Multidimensional Arrays:
+let bestMealPlan: string[][] = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
+let bestBooleansTwice: boolean[][] = [bestBooleans, bestBooleans];
+let numbersMulti: number[][][] = [ [[1],[2,3]], [[7],bestNumbers] ];
+
 export {}
