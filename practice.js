079d5fe12bc6e4817aa35cb741e5017f280bfd2d
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-08-24 19:39:01 -0400

    more objects

--- practice.js
+++ practice.js
@@ -183,3 +183,25 @@ var ordersArrayTS2 = [
     [Pet2.Hamster, 2],
     [Pet2.Chinchilla, 50]
 ];
+//objects
+function sayHappyBirthdayWithObject(personObject) {
+    var output = '';
+    output += 'Happy Birthday '
+        + personObject.name + '! ';
+    output += 'You are now '
+        + personObject.age + ' years old! ';
+    output += 'Your birthday wish was to receive '
+        + personObject.giftWish
+        + '. And guess what? You will ';
+    if (!personObject.success) {
+        output += 'not ';
+    }
+    output += 'receive it! \n';
+    console.log(output);
+}
+var birthdayBabies = [
+    { name: 'Liam', age: 0, giftWish: 'karate skills', success: false },
+    { name: 'Olivia', age: 0, giftWish: 'a bright future', success: true },
+    { name: 'Ava', age: 0, giftWish: '$0.25', success: true }
+];
+birthdayBabies.forEach(sayHappyBirthdayWithObject);
